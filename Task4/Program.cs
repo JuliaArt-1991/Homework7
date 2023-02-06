05143dbfb45ba9cece353734a0a3e9f20ee0574f
Julia <artamon-julia@ya.ru>
2023-02-06 21:42:37 +0700

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,47 @@
+﻿// Транспонирование
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void TransposedMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0) / 2; i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int a = matrix[i, j];
+            matrix[i, j] = matrix[matrix.GetLength(0) - i - 1, j];
+            matrix[matrix.GetLength(0) - i - 1, j] = a;
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine();
+TransposedMatrix(matrix);
+PrintMatrix(matrix);
