fa04c9cd6944b949bd7121dd02bb29d6a7b69f50
Julia <artamon-julia@ya.ru>
2023-02-06 21:38:33 +0700

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// 47
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble()*10, 2); 
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
