2ef01f051fc9df39bd3341359110f212a231adc1
Julia <artamon-julia@ya.ru>
2023-02-06 21:41:08 +0700

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿// 52
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageValue(int[,] matrix)
+{
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double result = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            {
+                result += matrix[i, j];
+            }
+        }
+        Console.Write($"{Math.Round(result / matrix.GetLength(1),2)};  ");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+AverageValue(matrix);
